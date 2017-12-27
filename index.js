7cd421760086ce915f1920dd97a88fabca928d99
weeznog <drew.m.peterson@gmail.com>
2017-12-27 10:30:39 -0800

    intial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'there' });
+});
+
+app.listen(PORT, err => {
+	if (err) {
+		console.log('server error', err);
+	} else {
+		console.log('listening on port', PORT);
+	}
+});
