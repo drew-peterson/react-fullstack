92bedee7cf49259d39577a7ff0a0bf006d0c8a76
weeznog <drew.m.peterson@gmail.com>
2017-12-28 11:57:22 -0800

    add prod keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// heroku specificially sets this node_env
+if (process.env.NODE_ENV === 'production') {
+	// we are in production
+	module.exports = require('./prod');
+} else {
+	// return dev keys
+	module.exports = require('./dev'); // pull in dev keys and export them
+}
