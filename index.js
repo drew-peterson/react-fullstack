d04ee9d44349a8d5eb1dcee73378b11ff7fa4ad5
weeznog <drew.m.peterson@gmail.com>
2017-12-28 10:44:53 -0800

    oauth added to server

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/User'); // require mongoose models -- model has to exist first before passport can use it
 require('./services/passport'); // make sure passport is ran...
 
+mongoose.connect(keys.MONGO_URI);
+
 const app = express();
+
+// use cookies inside app
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, // cookie lasts 30days in milliseconds,
+		keys: [keys.COOKIE_KEY]
+	})
+);
+
+// tell passport to use cookies...
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app); // bring in authRoutes function and call it with app
 
--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,27 @@
 // OAuth
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
 
+const User = mongoose.model('user'); // bring in mongo class
+
+// generate unqiue indenityfing info
+// turn mongoose model into id
+passport.serializeUser((user, done) => {
+	done(null, user.id); // user.id is not _oAuthId --> referring to user._id (mongo id) for specific user
+});
+// id: is the above user.id
+// turn id into mongoose model
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
+// Google
+// https://developers.google.com/identity/one-tap/web/get-started -- new google login??
 passport.use(
 	new GoogleStrategy(
 		{
@@ -11,11 +30,40 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken', accessToken);
-			console.log('refreshToken', refreshToken);
-			console.log('profile', profile);
+			newOrExistingUser(profile.id, done);
 		}
 	)
 );
 
-// https://developers.google.com/identity/one-tap/web/get-started -- new google login??
+// Facebook
+passport.use(
+	new FacebookStrategy(
+		{
+			clientID: keys.FACEBOOK_APP_ID,
+			clientSecret: keys.FACEBOOK_APP_SECRET,
+			callbackURL: '/auth/facebook/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {}
+	)
+);
+
+function newOrExistingUser(_oAuthId, done) {
+	User.findOne({ _oAuthId }).then(existingUser => {
+		if (existingUser) {
+			done(null, existingUser); // calls passport.serializeUser and passes existingUser as first arg -> user
+		} else {
+			// create new user...
+			new User({ _oAuthId }).save().then(user => {
+				done(null, user);
+			});
+		}
+	});
+}
+
+// JWT
+// Using JWT's in the header of each request in the other course was a result of putting the react app on one domain and the API server on a different one.  In a few lectures we dive really deep into talking about why the server setup in this course makes working with cookies possible.  One of the nasty things around JWT's is that there isn't a great place to store them on the client side - they are almost always weak against XSS attacks.  Using cookies solves that huge huge issue.
+
+// cookeies --> we dont have to worry about them on the react side of
+
+// COOKIES
+// request from browser after logged in > cookie extracts user id and in req we get req.user
