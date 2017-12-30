c8cdb156e68a8cbdda916352855ac6fcd8976a09
weeznog <drew.m.peterson@gmail.com>
2017-12-30 10:06:11 -0800

    add styled-components + fix stripe key bug

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import { ThemeProvider } from 'styled-components';
+
+const colors = {
+	main: '#393276',
+	dark: '#0D083B',
+	light: '#837EB1'
+};
 const Landing = () => {
+	const Landing = styled.div`
+		text-align: center;
+		color: ${props => props.theme.main};
+		background-color: ${props => props.theme.dark};
+	`;
+
 	return (
-		<div style={{ textAlign: 'center' }}>
-			<h1>Emaily!</h1>
-			collect feedback from your users
-		</div>
+		<ThemeProvider theme={colors}>
+			<Landing>
+				<h1>Emaily!</h1>
+				collect feedback from your users
+			</Landing>
+		</ThemeProvider>
 	);
 };
 
