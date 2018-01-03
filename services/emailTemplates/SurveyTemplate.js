07a858a3cd125243375054e32494e390525fddb1
weeznog <drew.m.peterson@gmail.com>
2018-01-03 15:11:31 -0800

    d

--- services/emailTemplates/SurveyTemplate.js
+++ services/emailTemplates/SurveyTemplate.js
@@ -14,9 +14,6 @@ module.exports = survey => {
 					<div>
 						<a href="${keys.REDIRECT_DOMAIN}/api/surveys/${survey.id}/no">No</a>
 					</div>
-					<div>
-						<a href="https://google.com">google test</a>
-					</div>
 				</div>
 			</body>
 		</html>
